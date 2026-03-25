cb91c5540f278f8f21d2977a9601c78c6228ec73
mayur0318 <mayurparmar0317@gmail.com>
2026-03-25 11:47:22 +0530

    CRUD operations for Customer and Vendor models

--- wearweb-backend/src/controllers/CustomerController.js
+++ wearweb-backend/src/controllers/CustomerController.js
@@ -14,6 +14,61 @@ const getAllCustomer = async (req, res) => {
     });
   }
 };
+
+const getCustomerById = async (req, res) => {
+  try {
+    const getCustomer = await customerSchema.findById(req.params.id);
+    res.status(200).json({
+      message: "Customer",
+      data: getCustomer,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error while fetching customer",
+      err: err,
+    });
+  }
+};
+
+const updateCustomer = async (req, res) => {
+  try {
+    const updateCustomerObj = await customerSchema.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true },
+    );
+    res.status(200).json({
+      message: "Customer updated successfully",
+      data: updateCustomerObj,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error whihle updating customer",
+      err: err,
+    });
+  }
+};
+
+const deleteCustomer = async (req, res) => {
+  try {
+    const deleteCustomerObj = await customerSchema.findByIdAndDelete(
+      req.params.id,
+    );
+    res.status(200).json({
+      message: "Customer deleted successfully",
+      data: deleteCustomerObj,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error while deleting customer",
+      err: err,
+    });
+  }
+};
+
 module.exports = {
   getAllCustomer,
+  getCustomerById,
+  updateCustomer,
+  deleteCustomer,
 };
--- wearweb-backend/src/routes/CustomerRoutes.js
+++ wearweb-backend/src/routes/CustomerRoutes.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const customerController = require("../controllers/CustomerController");
 
 router.get("/customers", customerController.getAllCustomer);
+router.get("/customer/:id", customerController.getCustomerById);
+router.put("/customer/:id", customerController.updateCustomer);
+router.delete("/customer/:id", customerController.deleteCustomer);
 
 module.exports = router;
